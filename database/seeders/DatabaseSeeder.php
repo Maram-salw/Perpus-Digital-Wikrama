b27da2601990f33c9ae305e3f5ce3ff8c38cb7d2
aryyyyy31 <aryadevitra97@gmail.com>
2024-02-07 11:18:13 +0700

    welcome

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(LaratrustSeeder::class);
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+        ])->attachRole('Admin');
+
+        \App\Models\User::create([
+            'name' => 'Petugas',
+            'email' => 'petugas@gmail.com',
+            'password' => Hash::make('123'),
+        ])->attachRole('Petugas');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// autentikasi Users
+Route::middleware('auth')->group(function () {
+
+    // menu utama
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    // hanya admin yang bisa akses
+    Route::middleware('role:Admin')->group(function () {
+
+        Route::get('/admin', function () {
+            return view('components.master');
+        })->name('admin');
+
+    });
+
+    // hanya petugas yang bisa akses
+    Route::middleware('role:Petugas')->group(function () {
+
+        Route::get('/petugas', function () {
+            return view('components.master');
+        })->name('petugas');
+
+    });
+
+});
